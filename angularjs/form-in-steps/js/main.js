766f35798453397dc9192e67e501f03408525385
RONEY-AS\Roney AS <roney.lacerda@actualsales.com.br>
2017-03-12 17:12:36 -0300

    add angular form-in-steps

---
+++ angularjs/form-in-steps/js/main.js
@@ -0,0 +1,50 @@
+// Cria a app e injeta ngAnimate e ui-router 
+// =============================================================================
+angular.module('formApp', ['ngAnimate', 'ui.router'])
+
+// configura rotas 
+// =============================================================================
+.config(function($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+
+  // rota para o form (/form)
+    .state('form', {
+    url: '/form',
+    templateUrl: 'partial/form.html',
+    controller: 'formController'
+  })
+
+  // cada uma das sessões tem sua url (/form/<nome>)
+  .state('form.signup', {
+    url: '/signup',
+    templateUrl: 'partial/form-signup.html'
+  })
+
+  .state('form.profile', {
+    url: '/profile',
+    templateUrl: 'partial/form-profile.html'
+  })
+
+  .state('form.address', {
+    url: '/address',
+    templateUrl: 'partial/form-address.html'
+  });
+
+  // url desconhecida = volta para /form
+  $urlRouterProvider.otherwise('/form/signup');
+})
+
+// controller
+// =============================================================================
+.controller('formController', function($scope) {
+
+  // armazena os dados do form
+  $scope.formData = {};
+
+  // function que processa o form
+  $scope.processForm = function() {
+    alert('awesome!');
+  };
+
+});
