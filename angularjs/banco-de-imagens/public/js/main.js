2db5bcf848b94e96db1755d764748a25a3ae8618
Roney Lacerda <roney.lacerda@actualsales.com.br>
2017-03-25 22:57:04 -0300

    angularjs imagebank set ngRoute and ng-view

--- angularjs/banco-de-imagens/public/js/main.js
+++ angularjs/banco-de-imagens/public/js/main.js
@@ -1,1 +1,23 @@
-angular.module('imagebank', ['MinhasDiretivas', 'ngAnimate']);
+angular.module('imagebank', ['minhasDiretivas', 'ngAnimate', 'ngRoute'])
+  .config(function($routeProvider, $locationProvider) {
+
+    $locationProvider.html5Mode(true);
+
+    $routeProvider.when('/fotos', {
+      templateUrl: 'partials/principal.html',
+      controller: 'FotosController'
+    });
+
+    $routeProvider.otherwise({
+      redirectTo: '/fotos'
+    });
+
+    $routeProvider.when('/fotos/new', {
+      templateUrl: 'partials/foto.html'
+    });
+
+    $routeProvider.otherwise({
+      redirectTo: '/fotos'
+    });
+
+  });
